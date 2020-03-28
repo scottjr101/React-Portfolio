590bd0532480b1e906fc338dfa9d9bbe6797d3fb
Scott Ladd <scottjr101@gmail.com>
2020-03-27 23:51:59 -0400

    production path

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ const server = require('http').createServer(app);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 server.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
   });
