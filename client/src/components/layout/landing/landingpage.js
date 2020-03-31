06da823ebd5c8e5f484a7c8b44a23596b328e79a
Scott Ladd <scottjr101@gmail.com>
2020-03-31 02:13:32 -0400

    Added custom enter button to landing page

--- client/src/components/layout/landing/landingpage.js
+++ client/src/components/layout/landing/landingpage.js
@@ -8,10 +8,13 @@ const Landing = () => {
         <div className='container d-flex h-100 align-items-center'>
           <div className='mx-auto text-center'>
             <h1 className='mx-auto my-0'>Scott Ladd</h1>
-            
-            <a href='#about' className='js-scroll-trigger enter'>
-              Enter
-            </a>
+            <div className='container d-flex justify-content-center px-0'>
+              <a href='#about' className='js-scroll-trigger'>
+                <div className='A-btn btn-two'>
+                  <span>Enter</span>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </header>
