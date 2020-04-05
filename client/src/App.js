9547163e0932ca7612c498088c837df74d27e91e
Scott Ladd <scottjr101@gmail.com>
2020-04-05 04:36:59 -0400

    added weather context

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { WeatherProvider } from './context/weather/context';
 import Landing from './components/layout/landing/landingpage';
 import Routes from './routing/routes';
 import './App.css';
@@ -8,10 +9,12 @@ const App = () => {
   return (
     <Router>
       <Fragment>
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Route component={Routes} />
-        </Switch>
+        <WeatherProvider>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </WeatherProvider>
       </Fragment>
     </Router>
   );
