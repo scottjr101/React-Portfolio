2ca6ee44cd18be5fa7ff058f539e6a2a180ca7b9
Scott Ladd <scottjr101@gmail.com>
2020-04-05 04:36:33 -0400

    added weather route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+require('dotenv').config();
+const ApiKey = process.env.APIKey;
 
 const app = express();
 
@@ -11,14 +14,29 @@ const server = require('http').createServer(app);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+// Define API routes here
+app.post('/weather', async (req, res) => {
+  const { latitude, longitude } = req.body;
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${ApiKey}`
+    );
+    const data = await response.data;
+
+    return res.json(data);
+  } catch (error) {
+    console.log(error);
   }
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 server.listen(PORT, () => {
-    console.log(`🌎 ==> API server now on port ${PORT}!`);
-  });
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
