dd6afc531ea0496fb87ef00adb039b70cdc1bfbb
Scott Ladd <scottjr101@gmail.com>
2020-04-08 04:23:20 -0400

    removed fontawesome cdn, and custom weather icons

--- client/src/components/layout/footer/footer.js
+++ client/src/components/layout/footer/footer.js
@@ -1,11 +1,18 @@
 import React, { Fragment } from 'react';
-import Weather from "../weather/weather";
+import { Icon } from '@iconify/react';
+import fileDocument from '@iconify/icons-mdi/file-document';
+import emailSend from '@iconify/icons-mdi/email-send';
+import cellphoneIphone from '@iconify/icons-mdi/cellphone-iphone';
+import linkedinIcon from '@iconify/icons-mdi/linkedin';
+import redditIcon from '@iconify/icons-mdi/reddit';
+import githubFaceIcon from '@iconify/icons-mdi/github-face';
+import Weather from '../weather/weather';
 
 const Footer = () => {
   return (
     <Fragment>
       {/* Signup Section */}
-      <section id='signup' className='signup-section'>
+      <section id='contact' className='signup-section'>
         <Weather />
       </section>
       {/* Contact Section */}
@@ -15,8 +22,8 @@ const Footer = () => {
             <div className='col-md-4 mb-3 mb-md-0'>
               <div className='card py-4 h-100'>
                 <div className='card-body text-center'>
-                  <i className='font-size-6 fas fa-file-word text-primary mb-2' />
-                  <h4 className='text-uppercase m-0'>Resume</h4>
+                  <Icon icon={fileDocument} color='#64a19d' height={23} />
+                  <h4 className='text-uppercase mt-2 mb-0'>Resume</h4>
                   <hr className='my-4' />
                   <div className='text-black-50'>
                     <a href='https://docs.google.com/file/d/1QOsbAmm9rI3eX5PtsUy2ynAToHJxshOc/view'>
@@ -29,8 +36,8 @@ const Footer = () => {
             <div className='col-md-4 mb-3 mb-md-0'>
               <div className='card py-4 h-100'>
                 <div className='card-body text-center'>
-                  <i className='font-size-6 fas fa-envelope text-primary mb-2' />
-                  <h4 className='text-uppercase m-0'>Email</h4>
+                  <Icon icon={emailSend} color='#64a19d' height={23} />
+                  <h4 className='text-uppercase mt-2 mb-0'>Email</h4>
                   <hr className='my-4' />
                   <div className='text-black-50'>
                     <a href='mailto:scottjr101@gmail.com'>
@@ -43,26 +50,26 @@ const Footer = () => {
             <div className='col-md-4 mb-3 mb-md-0'>
               <div className='card py-4 h-100'>
                 <div className='card-body text-center'>
-                  <i className='font-size-6 fas fa-mobile-alt text-primary mb-2' />
-                  <h4 className='text-uppercase m-0'>Phone</h4>
+                  <Icon icon={cellphoneIphone} color='#64a19d' height={23} />
+                  <h4 className='text-uppercase mt-2 mb-0'>Phone</h4>
                   <hr className='my-4' />
                   <div className='text-black-50'>+1 (404) 435-7803</div>
                 </div>
               </div>
             </div>
           </div>
-          <div className='social d-flex justify-content-center font-size-6'>
+          <div className='social d-flex justify-content-center'>
             <a
               href='https://www.linkedin.com/in/scott-ladd-1988abc/'
               className='mx-2'
             >
-              <i className='fab fa-linkedin' />
+              <Icon icon={linkedinIcon} height={33} />
             </a>
             <a href='https://reddit.com/u/Scottjr101' className='mx-2'>
-              <i className='fab fa-reddit-alien' />
+              <Icon icon={redditIcon} height={33} />
             </a>
             <a href='https://github.com/scottjr101' className='mx-2'>
-              <i className='fab fa-github-alt' />
+              <Icon icon={githubFaceIcon} height={33} />
             </a>
           </div>
         </div>
