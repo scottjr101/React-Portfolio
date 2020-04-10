1650e9568cb09e7e404826aef79ad59cd6f0678c
Scott Ladd <scottjr101@gmail.com>
2020-04-10 03:58:45 -0400

    404 state setup, fixed port header

--- client/src/components/layout/nomatch/noMatch.js
+++ client/src/components/layout/nomatch/noMatch.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import Cat from '../../../assets/imgs/404-cat.jpg';
 
 const NoMatch = () => {
+  const [facts, setFacts] = useState([]);
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  // Grab Latitude and Longitude from user on page load.
+  useEffect(() => {
+    fetch('https://catfact.ninja/facts', options)
+      .then((res) => res.json())
+      .then((result) => setFacts([result]))
+      .catch((error) => console.log(error));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className='masthead2'>
-      <div className='container d-flex h-100 align-items-center'>
-        <div className='mx-auto text-center'>
-          <h1 className='mx-auto my-0'>404 Page Not Found</h1>
-          <h1 className='mx-auto mt-2 mb-5'>
-            <span role='img' aria-label='Face With Rolling Eyes Emoji'>
-              🙄
-            </span>
-          </h1>
+    <Fragment>
+      <div className='FadedHeader' />
+      <div className='container-fluid p-5 bg-light'>
+        <div className='row d-flex justify-content-center'>
+          <div className='col d-flex justify-content-center mb-5'>
+            <h1 className='NotFound m-0'>404 Page Not Found</h1>
+          </div>
+        </div>
+        <div className='row justify-content-center'>
+          <div className='col-12 col-md-10 col-lg-6'>
+            <img className='img-fluid rounded' src={Cat} alt='Cat' />
+          </div>
+          <div className='col-12 col-md-10 col-lg-6 p-3 p-lg-2'>
+            <h3 className='text-center mb-4'>
+              You stumbled upon a bad route, but since you're here... take a
+              look at this cat and read a random Cat Fact!
+            </h3>
+            <hr className='break' />
+            {facts.map((D) => (
+              <Fragment key={D}>
+                <h4 className='text-center text-black-50 mt-4'>
+                  "{D.data[0].fact}"
+                </h4>
+              </Fragment>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
