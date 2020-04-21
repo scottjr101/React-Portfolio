474908bb7c088d0714e590c8392910a773148b1f
Scott Ladd <scottjr101@gmail.com>
2020-04-21 17:11:23 -0400

    zipcode route

--- server.js
+++ server.js
@@ -37,6 +37,26 @@ app.post('/weather', async (req, res) => {
   }
 });
 
+app.post('/weather-zipcode', async (req, res) => {
+  // Pull Lat and Long out of HTTP headers
+  const { zipcode } = req.body;
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&units=imperial&appid=${ApiKey}`
+    );
+    const data = await response.data;
+    // Debugging
+    console.log(`OpenWeatherMap API Call Successful!`);
+    // Send weather data from Openweather back to client side
+    return res.json(data);
+    // Catch any errors
+  } catch (error) {
+    console.log(
+      `OpenWeatherMap API Call Failed, This Is Why ==> '${error.response.statusText}'`
+    );
+  }
+});
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
